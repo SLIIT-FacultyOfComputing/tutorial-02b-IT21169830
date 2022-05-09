185eb3b64dcbf88340cc190c133741c730c37634
WOOHI84 <97064107+IT21169830@users.noreply.github.com>
2022-05-09 20:21:56 +0000

    IT21169830 Tutorial 02

--- tute04.cpp
+++ tute04.cpp
@@ -24,6 +24,17 @@ int main() {
   std::cin >> r;
   std::cout << "nCr = ";
   std::cout << nCr(n,r);
-  std::cout << std::endl;
-  return 0;
+  std::cout << std::endl;  
+}
+long Factorial(int no){
+  long fac=1;
+  for (int r=no; r >= 1; r--) {
+        fac = fac * r;
+  }
+  return fac;
+}
+
+long nCr(int n, int r){
+  long nCr = Factorial(n)/ (Factorial(r) * Factorial(n-r));
+  return nCr; 
 }
